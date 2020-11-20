93414a5ea69300630896cdd9ca8062fbd74c0003
OzlemTS <ozlemturkerii@gmail.com>
2020-11-20 17:01:01 +0300

    starter is ok

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,28 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="min-h-screen flex items-center bg-purple-500">
+        <div className="flex-1 max-w-4xl mx-auto p-10">
+          <ul className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
+            <li className="bg-white rounded-lg shadow-md">
+              <div className="h-32"></div>
+            </li>
+            <li className="bg-white rounded-lg shadow-md">
+              <div className="h-32"></div>
+            </li>
+            <li className="bg-white rounded-lg shadow-md">
+              <div className="h-32"></div>
+            </li>
+            <li className="bg-white rounded-lg shadow-md">
+              <div className="h-32"></div>
+            </li>
+            <li className="bg-white rounded-lg shadow-md">
+              <div className="h-32"></div>
+            </li>
+            <li className="bg-white rounded-lg shadow-md">
+              <div className="h-32"></div>
+            </li>
+          </ul>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </div>
   )
 }
---
+++ pages/prices.js
@@ -0,0 +1,5 @@
+export default function Prices() {
+	return (
+		<div>Prices</div>
+	);
+}
