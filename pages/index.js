0df587f114e69236157e4f0b312382ad0ccfaf83
OzlemTS <ozlemturkerii@gmail.com>
2020-11-23 17:57:04 +0300

    img is added

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,48 @@ export default function Home() {
         <div className="flex-1 max-w-4xl mx-auto p-10">
           <ul className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
             <li className="bg-white rounded-lg shadow-md">
-              <div className="h-32"></div>
+              <div className="h-32">
+                <a href={"prices"}>
+                  <img className="" src="430.jpg"></img>
+                </a>
+              </div>
             </li>
             <li className="bg-white rounded-lg shadow-md">
-              <div className="h-32"></div>
+              <div className="h-32">
+                <a href={"prices"}>
+                  <img className="" src="430.jpg"></img>
+                </a>
+              </div>
             </li>
             <li className="bg-white rounded-lg shadow-md">
-              <div className="h-32"></div>
+              <div className="h-32">
+                <a href={"prices"}>
+                  <img className="" src="430.jpg"></img>
+                </a>
+              </div>
             </li>
             <li className="bg-white rounded-lg shadow-md">
-              <div className="h-32"></div>
+              <div className="h-32">
+                <a href={"prices"}>
+                  <img className="" src="430.jpg"></img>
+                </a>
+              </div>
             </li>
             <li className="bg-white rounded-lg shadow-md">
-              <div className="h-32"></div>
+              <div className="h-32">
+                <a href={"prices"}>
+                  <img className="" src="430.jpg"></img>
+                </a>
+              </div>
             </li>
             <li className="bg-white rounded-lg shadow-md">
-              <div className="h-32"></div>
+              <div className="h-32">
+                <a href={"prices"}>
+                  <img className="" src="430.jpg"></img>
+                </a>
+              </div>
             </li>
+
           </ul>
         </div>
       </div>
